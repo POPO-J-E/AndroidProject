04d8274dff06381d68d08666eb953b8261229717
Etienne DEBARD <etienne.debard@etu.univ-lyon1.fr>
2017-10-04 11:27:51 +0200

    add project

---
+++ app/src/main/java/androidproject/ddsociety/com/polytech_2017_5a_initial_dolle_debard/MainActivity.java
@@ -0,0 +1,13 @@
+package androidproject.ddsociety.com.polytech_2017_5a_initial_dolle_debard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
