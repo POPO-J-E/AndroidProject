bf5d5ba91d8397e27e18aa438bddbaf78c5c07f9
DOLLE <jeremy.dolle34@gmail.com>
2017-10-11 16:56:17 +0200

    Add fragment training (switch btw 2 fragments)

--- app/src/main/java/androidproject/ddsociety/com/polytech_2017_5a_initial_dolle_debard/MainActivity.java
+++ app/src/main/java/androidproject/ddsociety/com/polytech_2017_5a_initial_dolle_debard/MainActivity.java
@@ -1,13 +1,34 @@
 package androidproject.ddsociety.com.polytech_2017_5a_initial_dolle_debard;
 
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MenuFragment.OnFragmentInteractionListener, QuestionFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        QuestionFragment fragment = new QuestionFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frangment_container, fragment);
+        transaction.commit();
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    public void changeFragment(){
+        MenuFragment newFragment = new MenuFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_right);
+
+        transaction.replace(R.id.frangment_container, newFragment);
+        transaction.commit();
     }
 }
